7c02ca1cd17d55419ff8586c7a490768c21c017f
zixiang <lixuezixiang@gmail.com>
2016-11-03 01:57:22 -0700

    add prompt displaying

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,42 @@
-#include <stdlib.h>
-#include <iostream>
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <assert.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include "parsing.h"
-
+#include "main.h"
 #define TRUE 1
 
 using namespace std;
 
+
+const int max_name_len = 256;
+const int max_path_len = 1024;
+
+void getprompt(char *prompt)
+{
+    struct passwd *pwd;
+    char hostname[max_name_len];
+    char pathname[max_path_len];
+    int length;
+    pwd = getpwuid(getuid());
+    getcwd(pathname, max_path_len);
+    if(gethostname(hostname, max_name_len)==0)
+        sprintf(prompt,"[Rshell]%s@%s:",pwd->pw_name,hostname);
+    else
+        sprintf(prompt,"[Rshell]%s@unknown:",pwd->pw_name);
+    length = strlen(prompt);
+    if(strlen(pathname) < strlen(pwd->pw_dir) || strncmp(pathname,pwd->pw_dir,strlen(pwd->pw_dir))!=0)
+        sprintf(prompt + length,"%s",pathname);
+    else
+        sprintf(prompt + length,"~%s", pathname+strlen(pwd->pw_dir));
+    length = strlen(prompt);
+    if(geteuid() == 0)
+        sprintf(prompt + length,"#");
+    else
+        sprintf(prompt + length,"$");
+    return;
+}
+
+
+
 int main(int argc, char* argv[]){
 	int status = 0;
+	char prompt[128];
 	char cmd[32];
 	char* parameter[1024];
 	char connector[10];
@@ -22,6 +44,8 @@ int main(int argc, char* argv[]){
 	while(TRUE) {
 		int rslt = 0;
 		string line;
+		getprompt(prompt);
+		cout<<prompt<<">>";
 		getline(cin,line);
 		Parsing* p = new Parsing(line);  
 		p->readCommands();
---
+++ src/main.h
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h> 
+#include <errno.h>
+#include <pwd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h> 
+#include "parsing.h"
